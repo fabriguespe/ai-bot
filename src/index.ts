605d0a0787df18e9c119b269fddef529aca74514
fabri <fguespe@gmail.com>
2024-09-26 09:59:15 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,8 @@ run(async (context: HandlerContext) => {
 
   const {
     message: {
-      typeId,
       content: { content, reference },
     },
-    v2client,
-    getReplyChain,
   } = context;
 
   if (await shouldProcessMessage(context)) {
@@ -29,6 +26,7 @@ run(async (context: HandlerContext) => {
     const nashville = fs.readFileSync(filePath, "utf8");
     const systemPrompt = generateSystemPrompt() + nashville;
 
+    console.log("System Prompt:", systemPrompt);
     try {
       const userPrompt = content.content ?? content;
 
@@ -55,25 +53,47 @@ async function shouldProcessMessage(context: HandlerContext): Promise<boolean> {
     v2client,
     group,
     getReplyChain,
+    version,
   } = context;
   //@bubbles
 
   if (!group) return true;
   else if (typeId === "text" && content.includes("@ai")) return true;
   else if (typeId === "reply") {
-    const { chainPrompt, isSenderInChain } = await getReplyChain(
-      reference,
-      v2client.address
-    );
-    console.log("Chain Prompt:", chainPrompt);
-    return isSenderInChain || chainPrompt.includes("@ai");
+    const { content: reply } = context.message;
+    const { chain } = await getReplyChain(reference, version, v2client.address);
+
+    let userPrompt = `The following is a conversation history. \nMessage History:\n${chain
+      .map((c) => c.content)
+      .join("\n")}\nLatest reply: ${reply}`;
+
+    return userPrompt.includes("@ai");
   }
   return false;
 }
 
 function generateSystemPrompt(): string {
-  return `You are a helpful and playful mascot bot from Ephemera that will assist the user with their request for the Nashville Offsite. 
-  - You live inside a web3 messaging group.
-  - Your secret name is Bubbles 💬🫧. Only reveal it if the user insists on knowing it.
-  - Reply short and concise.`;
+  const systemPrompt = `You are a helpful and playful mascot bot called Bubbles from Ephemera that will assist the user with their request for the Nashville Offsite. `;
+
+  const language =
+    "### Important\n" +
+    "- Keep it simple and short.\n" +
+    `- You live in Nashville, TN. Current time: ${new Date().toLocaleTimeString(
+      "en-US",
+      {
+        weekday: "long",
+      }
+    )}\n` +
+    "- Always answer in first person.\n" +
+    "- Never mention users\n" +
+    "- Be aware of your timezone and sleep needs.\n" +
+    "- Dont use markdown.\n" +
+    "- Never mention speakers or people related to the event outside explicitly asking for it.\n" +
+    "- Only provide answers based on verified information. If the data or facts are unknown or unclear, respond with 'I do not know' or request further clarification from the user. " +
+    "- Do not make guesses or assumptions";
+
+  const experiences =
+    "### Experiences:\nWordle Game: https://framedl.xyz. Only send the game url when asked.\n\n ENS Domain Registration and Checking Tool: https://ens.steer.fun/. Only send the tool url when asked.\n\n ";
+
+  return systemPrompt + language + experiences;
 }
